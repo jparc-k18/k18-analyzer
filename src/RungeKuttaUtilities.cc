512adc0b4ddd70ad14c4bb2754a2e10e82fa1628
Shuhei Hayakawa <shuhei.hayakawa.c8@tohoku.ac.jp>
2026-08-17 13:54:17 +0900

    Fix argument order of RK field sampling points Z2/Z4 and StepPoint allocation

--- src/RungeKuttaUtilities.cc
+++ src/RungeKuttaUtilities.cc
@@ -288,9 +288,9 @@ RK::TraceOneStep(Double_t StepSize, const RKTrajectoryPoint &prevPoint)
     RK::CalcDeltaFieldIntegral(prevPoint, f1);
 
   ThreeVector Z2 = Z1 +
-    ThreeVector(0.5*dr,
-                0.5*dr*pre_u + 0.125*dr*dr*f1.kx,
-                0.5*dr*pre_v + 0.125*dr*dr*f1.ky);
+    ThreeVector(0.5*dr*pre_u + 0.125*dr*dr*f1.kx,
+                0.5*dr*pre_v + 0.125*dr*dr*f1.ky,
+                0.5*dr);
   ThreeVector B2 = gField.GetField(Z2);
 #ifdef ExactFFTreat
   ThreeVector dBdX2 = gField.GetdBdX(Z2);
@@ -323,9 +323,9 @@ RK::TraceOneStep(Double_t StepSize, const RKTrajectoryPoint &prevPoint)
     RK::CalcDeltaFieldIntegral(prevPoint, f3, df2, df1, 0.5*dr);
 
   ThreeVector Z4 = Z1 +
-    ThreeVector(dr,
-                dr*pre_u + 0.5*dr*dr*f3.kx,
-                dr*pre_v + 0.5*dr*dr*f3.ky);
+    ThreeVector(dr*pre_u + 0.5*dr*dr*f3.kx,
+                dr*pre_v + 0.5*dr*dr*f3.ky,
+                dr);
   ThreeVector B4 = gField.GetField(Z4);
 #ifdef ExactFFTreat
   ThreeVector dBdX4 = gField.GetdBdX(Z4);
@@ -478,9 +478,9 @@ RK::PropagateOnce(Double_t StepSize, const RKTrajectoryPoint &prevPoint)
     RK::CalcDeltaFieldIntegral(prevPoint, f1);
 
   ThreeVector Z2 = Z1 +
-    ThreeVector(0.5*dr,
-                0.5*dr*pre_u + 0.125*dr*dr*f1.kx,
-                0.5*dr*pre_v + 0.125*dr*dr*f1.ky);
+    ThreeVector(0.5*dr*pre_u + 0.125*dr*dr*f1.kx,
+                0.5*dr*pre_v + 0.125*dr*dr*f1.ky,
+                0.5*dr);
   ThreeVector B2 = gField.GetField(Z2);
   ThreeVector dBdX2 = gField.GetdBdX(Z2);
   ThreeVector dBdY2 = gField.GetdBdY(Z2);
@@ -499,9 +499,9 @@ RK::PropagateOnce(Double_t StepSize, const RKTrajectoryPoint &prevPoint)
     RK::CalcDeltaFieldIntegral(prevPoint, f3, df2, df1, 0.5*dr);
 
   ThreeVector Z4 = Z1 +
-    ThreeVector(dr,
-                dr*pre_u + 0.5*dr*dr*f3.kx,
-                dr*pre_v + 0.5*dr*dr*f3.ky);
+    ThreeVector(dr*pre_u + 0.5*dr*dr*f3.kx,
+                dr*pre_v + 0.5*dr*dr*f3.ky,
+                dr);
   ThreeVector B4 = gField.GetField(Z4);
 
   ThreeVector dBdX4 = gField.GetdBdX(Z4);
@@ -767,8 +767,9 @@ RK::Trace(const RKCordParameter &initial, RKHitPointContainer &hitContainer)
   static const Double_t MaxPathLength  = 100000.; // mm
   static const Double_t NormalStepSize = -10.;   // mm
   Double_t MinStepSize = 2.;     // mm
-  /*for EventDisplay*/
-  std::vector<TVector3> StepPoint(MaxStep);
+  /* for EventDisplay: allocate only when it is actually used */
+  const Bool_t evDispReady = gEvDisp.IsReady();
+  std::vector<TVector3> StepPoint(evDispReady ? MaxStep : 0);
 
   Int_t iStep = 0;
   while(++iStep < MaxStep){
@@ -777,7 +778,8 @@ RK::Trace(const RKCordParameter &initial, RKHitPointContainer &hitContainer)
     RKTrajectoryPoint nextPoint = RK::TraceOneStep(StepSize, prevPoint);
 
     /* for EventDisplay */
-    StepPoint[iStep-1] = nextPoint.PositionInGlobal();
+    if(evDispReady)
+      StepPoint[iStep-1] = nextPoint.PositionInGlobal();
 
     while(RK::CheckCrossing(hitContainer[iPlane].first,
                             prevPoint, nextPoint,
@@ -884,12 +886,15 @@ RK::TraceToLast(RKHitPointContainer& hitContainer)
 
   iPlane += 1;
 
-  std::vector<TVector3> StepPoint(MaxStep);
+  /* for EventDisplay: allocate only when it is actually used */
+  const Bool_t evDispReady = gEvDisp.IsReady();
+  std::vector<TVector3> StepPoint(evDispReady ? MaxStep : 0);
   Int_t iStep = 0;
   while(++iStep < MaxStep){
     RKTrajectoryPoint nextPoint = RK::TraceOneStep(StepSize, prevPoint);
 
-    StepPoint[iStep-1] = nextPoint.PositionInGlobal();
+    if(evDispReady)
+      StepPoint[iStep-1] = nextPoint.PositionInGlobal();
 
     while(RK::CheckCrossing(hitContainer[iPlane].first,
                             prevPoint, nextPoint,
